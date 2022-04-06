bee427b17ad4808aeb7abb37ca94a20cda2bdc30
Amazagni <101255867+Amazagni@users.noreply.github.com>
2022-04-06 13:19:32 +0200

    Create longest_increasing_string.c

---
+++ longest_increasing_string.c
@@ -0,0 +1,23 @@
+#Longest increasing string
+def lis(A):
+    maxi = 0
+    n = len(A)
+    F = [1 for i in range(n)]
+    P = [-1 for i in range(n)]
+    for i in range(1,n):
+        for j in range(i):
+            if A[i] > A[j] and F[j]+1 > F[i]:
+                F[i] = F[j] + 1
+                P[i] = j
+            if F[i] > F[maxi]:
+                maxi = i
+    return maxi,F,P
+ 
+def printSol(A,P,i):
+    if P[i] != -1:
+        printSol(A,P,P[i])
+    print(A[i])
+tab = [2,1,4,3,4,8,5,7,2,0]
+i,F,P = lis(tab)
+print(P[i])
+printSol(tab,P,i)
